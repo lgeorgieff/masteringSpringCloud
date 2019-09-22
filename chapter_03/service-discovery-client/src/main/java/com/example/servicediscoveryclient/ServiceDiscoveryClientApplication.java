f80338101ed491d5d67a7e144d4b79d3f39db51c
Lukas Georgieff <lukas.georgieff@hotmail.com>
2019-09-22 22:25:55 +0200

    chapter 03: Add basic service-discovery-client

---
+++ chapter_03/service-discovery-client/src/main/java/com/example/servicediscoveryclient/ServiceDiscoveryClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicediscoveryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryClientApplication.class, args);
+	}
+
+}
