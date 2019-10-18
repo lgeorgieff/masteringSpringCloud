3142c740af8e86ecfa2a9b69699fd8319497df7c
Lukas Georgieff <lukas.georgieff@hotmail.com>
2019-10-18 22:48:39 +0200

    chapter 07: Add programmatic rule for echo-service-3

---
+++ chapter_07/gateway/src/main/kotlin/com/example/gateway/configuration/EchoRoutes.kt
@@ -0,0 +1,17 @@
+package com.example.gateway.configuration
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class EchoRoutes {
+    @Bean
+    fun customRouteLocator(builder: RouteLocatorBuilder) = builder.routes()
+        .route("echo-service-3") { r ->
+            r.path("/echo/3/{message}")
+                .filters { it.rewritePath("/echo/3/(?<message>)", "/echo/\${message}") }
+                .uri("http://localhost:8083")
+        }
+        .build()
+}
